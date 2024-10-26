397ea8edd8bee482cc8687862fae7a006bbcb0bb
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-10-26 11:25:25 +0100

    Update StreamArray.py

--- StreamArray.py
+++ StreamArray.py
@@ -37,20 +37,42 @@ class StreamArray:
 
     def __str__(self):
         return str(self.streams)
+    
+    def __len__(self):
+        return len(self.streams)
 
     def get_highest_resolution(self) -> Stream:
         video_streams = filter((lambda stream: not stream.is_audio), self.streams)
         ordered_by_res = sorted(video_streams,
                                 key=(lambda stream: int(stream.resolution[:-1]) if stream.resolution else 0),
                                 reverse=True)
-        return sorted(ordered_by_res, key=lambda stream: int(stream.frame_rate), reverse=True)[0]
+        if len(ordered_by_res) > 0:
+            ordered_by_frame_rate = sorted(ordered_by_res, key=lambda stream: int(stream.frame_rate), reverse=True)
+            ordered_by_hdr = sorted(ordered_by_frame_rate, key=(lambda stream: stream.is_hdr), reverse=True)
+            return ordered_by_hdr[0]
+
+    def get_audio_only(self) -> Stream:
+        aud_streams = filter((lambda stream: stream.is_audio), self.streams)
+        ordered_by_abr = sorted(aud_streams,
+                                key=(lambda stream: int(stream.abr[:-4]) if stream.abr else 0),
+                                reverse=True)
+        if len(ordered_by_abr) > 0:
+            return ordered_by_abr[0]
 
     def get_available_resolutions(self) -> tuple:
-        resolutions = [stream.resolution for stream in self.streams]
-        return tuple(sorted(resolutions, key=(lambda key: int(key[:-1]) if key else 0), reverse=True))
+        resolutions = [stream.resolution for stream in self.streams if stream.is_video]
+        return tuple(set(sorted(resolutions, key=(lambda key: int(key[:-1]) if key else 0), reverse=True)))
+    
+    def get_available_bit_rates(self) -> tuple:
+        bit_rate = [stream.bit_rate for stream in self.streams if stream.is_video]
+        return tuple(set(sorted(bit_rate, key=(lambda key: int(key[:-4]) if key else 0), reverse=True)))
 
+    def get_avaliable_frame_rates():
+        frame_rates = [stream.frame_rate for stream in self.streams if stream.is_video]
+        return tuple(set(frame_rates))
+      
     def get_audio(self) -> Stream:
-        return list(filter(lambda stream: stream.is_audio, self.streams))[0]
+        return list(filter(lambda stream: stream.is_audio, self.streams))
 
     def filter(self, **kwargs):
         reverse = kwargs.get('reverse')
@@ -68,6 +90,14 @@ class StreamArray:
                     raise KeyError(f'{key} not found in Stream Object')
         # print(filtered)
         return StreamArray(*filtered)
+    
+    def order_by(self, key):
+        if not all([hasattr(stream, key) for stream in self.streams]): return
+        return StreamArray(**sorted(self.streams, key=(lambda stream: getattr(stream, key)), reverse=True)))
+        
+    def first(self):
+        if len(self) > 0:
+            return self[0]
 
     def _append(self, *args):
         if all([isinstance(stream, Stream) for stream in args]):
