9f78b66bdf356f4963a610d4fd2265552e536738
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-10-26 11:42:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 echo "Installing playwright..."
 
-playwright install 
+if command -v playwright &>/dev/null; then
+    playwright install
+else
+    echo "Playwright is not installed. Please install Playwright."
+    exit 1
+fi
 
 echo "playwright installed"
